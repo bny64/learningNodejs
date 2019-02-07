c6f299d211c42f048cea95819551f1e5858e7b86
bny64 <bny64@naver.com>
2019-02-07 23:01:53 +0900

    about express module

--- nodeTestCode/20190204/expressTest/learn-express/app.js
+++ nodeTestCode/20190204/expressTest/learn-express/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var flash = require('connect-flash');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,12 +15,21 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(logger('dev')); //로그 남기는 기능
+app.use(express.static(path.join(__dirname, 'public'))); //경로 관리
+app.use(express.json()); //body-parser기능
+app.use(express.urlencoded({ extended: false })); //req.body가 json형태로 들어옴
+app.use(cookieParser('secret code')); //쿠키 관리
+app.use(session({
+  resave : false, //요청이 왔을 때 세션에 수정 사항이 생기지 않더라도 다시 저장 여부
+  saveUninitialized : false, //저장할 내역이 없더라도 세션을 저장할지 여부. 방문자 추적
+  secret:'secret code',
+  cookie:{
+    httpOnly:true,
+    secure:false
+  }
+}));
+app.use(flash());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
