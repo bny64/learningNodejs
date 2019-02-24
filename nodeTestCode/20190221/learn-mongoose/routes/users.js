2079ed56456cac5e4a83795fcbea9e6cd24a22bd
bny64 <bny64@naver.com>
2019-02-24 18:19:26 +0900

    mongoose

--- nodeTestCode/20190221/learn-mongoose/routes/users.js
+++ nodeTestCode/20190221/learn-mongoose/routes/users.js
@@ -1,9 +1,35 @@
 var express = require('express');
+var User = require('../schemas/user');
+
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+  try {
+    const users = await User.find();
+    res.json(users);
+  }catch(err){
+    console.error(err);
+    next(err);
+  }
 });
+
+router.post('/', async function(req, res, next){
+  try{
+    const user = new User({
+      name : req.body.name,
+      age : req.body.age,
+      married:req.body.married
+    });
+    
+    var result = await user.save();
+    console.log(result);
+    res.status(201).json(result);
+  }catch(err){
+    console.error(err);
+    next(err);
+  }
+  
+})
 
 module.exports = router;
