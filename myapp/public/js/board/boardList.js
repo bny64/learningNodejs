3b97a297769ba88f595c3775fb66fd5c4235a206
namyulbae <bny64@naver.com>
2019-05-07 18:11:36 +0900

    board

--- myapp/public/js/board/boardList.js
+++ myapp/public/js/board/boardList.js
@@ -3,6 +3,7 @@
 
     let pageNo = 1;
     let pageSize = 9;
+    let eleCount = 0;    
 
     getBoardList();
 
@@ -21,21 +22,39 @@
         xhr.send(formData);
     };
     
-    function makeBoardList(xhr){
-        if(xhr.status===200){
-            if(xhr.status === 200){
-                const result = JSON.parse(xhr.responseText).contents;
-                result.forEach((curEle)=>{
-                    const div = document.createElement('div');
-                    div.innerHTML = curEle.contents;
-                    curEle.contents = div.innerText; 
-                    console.log(curEle);    
-                });
-    
-            }else{
-                console.error(xhr.responseText);
+    function makeBoardList(xhr){        
+
+        if(xhr.status === 200){
+            
+            const result = JSON.parse(xhr.responseText).contents;
+            let html = '';
+            let i = 0;
+            for(; i<result.length; i++){
+                if(i%3==0) html += '<div class="row">';
+                
+                const div = document.createElement('div');
+                div.innerHTML = result[i].contents;
+                result[i].contents = div.innerText;
+
+                html += '<div class="col-md-4 col-sm-6 fh5co-tours animate-box" data-animate-effect="fadeIn">';
+                html += '<div href="#"><img src="/images/place-3.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">';
+                html += '<div class="desc" style="height:192px;">';
+                html += '<span></span>';
+                html += '<h3 class="mb20">' + result[i].title + '</h3>';
+                html += '<span class="mb20">' + result[i].name + '</span>';
+                html += '<span class="price mb20">' + result[i].contents.substring(0, 10) + '</span>';
+                html += '<a class="btn btn-primary btn-outline" href="#">상세 보기<i class="icon-arrow-right22"></i></a>';
+                html += '</div></div></div>';
+
+                if(i%3==2) html += '</div>';
             }
+
+            document.querySelector('.lastRow').insertAdjacentHTML('beforebegin', html);
+
+        }else{
+            console.error(xhr.responseText);
         }
+        
     };
 
 })();
