4cf8ff4fb5e223aa0a76f0d9f15f45c1f4c057cc
namyulbae <bny64@naver.com>
2019-05-31 18:04:55 +0900

    bshopper

--- b_shopper/routes/index.js
+++ b_shopper/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  res.render('main/index', { 
+    basedir : path.join(process.env.ROOT, 'views'),
+    title: 'Express' 
+  });
 });
 
 module.exports = router;
